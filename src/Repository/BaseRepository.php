bad02b7ab75488d68f8f67804c4f06f430dc9190
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-03-01 19:08:24 +0100

    base respository

---
+++ src/Repository/BaseRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace GraphAware\Neo4j\OGM\Repository;
+
+use GraphAware\Common\Result\RecordViewInterface;
+use GraphAware\Neo4j\Client\Formatter\Result;
+use GraphAware\Neo4j\OGM\Manager;
+use GraphAware\Neo4j\OGM\Metadata\ClassMetadata;
+
+class BaseRepository
+{
+    protected $classMetadata;
+
+    protected $manager;
+
+    protected $className;
+
+    public function __construct(ClassMetadata $classMetadata, Manager $manager, $className)
+    {
+        $this->classMetadata = $classMetadata;
+        $this->manager = $manager;
+        $this->className = $className;
+    }
+
+    public function findAll()
+    {
+        $label = $this->classMetadata->getLabel();
+        $query = sprintf('MATCH (n:%s) RETURN n', $label);
+
+        $result = $this->manager->getDatabaseDriver()->run($query);
+
+        return $this->hydrateResultSet($result);
+
+    }
+
+    public function hydrateResultSet(Result $result)
+    {
+        $entities = [];
+        foreach ($result->records() as $record) {
+                $entities[] = $this->hydrate($record);
+        }
+
+        return $entities;
+    }
+
+    public function hydrate(RecordViewInterface $record)
+    {
+        $reflClass = new \ReflectionClass($this->className);
+        $instance = $reflClass->newInstanceWithoutConstructor();
+
+        foreach ($this->classMetadata->getFields() as $field => $meta) {
+            if ($record->value('n')->hasValue($field)) {
+                if ($property = $reflClass->getProperty($field)) {
+                    $property->setAccessible(true);
+                    $property->setValue($instance, $record->value('n')->value($field));
+                }
+            }
+        }
+
+        return $instance;
+    }
+}
