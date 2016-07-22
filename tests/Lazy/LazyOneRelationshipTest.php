7a0076d424d34db77f853ef98500c471c293d556
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-07-22 22:52:29 +0200

    added test regarding SO issue

--- tests/Lazy/LazyOneRelationshipTest.php
+++ tests/Lazy/LazyOneRelationshipTest.php
@@ -5,6 +5,10 @@ namespace GraphAware\Neo4j\OGM\Tests\Lazy;
 use GraphAware\Neo4j\OGM\Tests\Integration\IntegrationTestCase;
 use GraphAware\Neo4j\OGM\Tests\Integration\Model\Company;
 use GraphAware\Neo4j\OGM\Tests\Integration\Model\User;
+use GraphAware\Neo4j\OGM\Tests\Lazy\Model\MetaResource;
+use GraphAware\Neo4j\OGM\Tests\Lazy\Model\Resources;
+use GraphAware\Neo4j\OGM\Tests\Lazy\Model\User as LazyUser;
+
 
 /**
  * Class LazyOneRelationshipTest
@@ -34,5 +38,38 @@ class LazyOneRelationshipTest extends IntegrationTestCase
         $friend = $user->getLoves()[0];
         $comp = $friend->getCurrentCompany();
         $this->assertEquals('Acme', $comp->getName());
+    }
+
+    /**
+     * @group lazy-joran
+     */
+    public function testCascadeTraversals()
+    {
+        $this->clearDb();
+        $meta = new MetaResource('wood');
+        $this->em->persist($meta);
+        $this->em->flush();
+        $this->em->clear();
+
+        $metaResource = $this->em->getRepository(MetaResource::class)->findOneBy('resourceType', 'wood');
+        $rWood = new Resources($metaResource);
+        $rWood->setResourceCount(20);
+        $user = new LazyUser();
+        $this->em->persist($user);
+        $this->em->persist($rWood);
+        $this->em->flush();
+        $user->addResource($rWood);
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $this->em->clear();
+
+        $users = $this->em->getRepository(LazyUser::class)->findAll();
+        foreach ($users as $user) {
+            foreach ($user->getResources() as $resource) {
+                $this->assertNotNull($resource);
+                $this->assertInstanceOf(MetaResource::class, $resource->getMetaResource());
+            }
+        }
     }
 }
