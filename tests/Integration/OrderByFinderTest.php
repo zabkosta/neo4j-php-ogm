ed9ee9d4cc802fb396575a4e2d1a687b280c2bc9
Christophe Willemsen <willemsen.christophe@gmail.com>
2017-02-26 16:44:32 +0100

    added test for order by in finders

---
+++ tests/Integration/OrderByFinderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GraphAware\Neo4j\OGM\Tests\Integration;
+
+use GraphAware\Neo4j\OGM\Tests\Integration\Models\Base\User;
+
+/**
+ * @group order-by-finder
+ */
+class OrderByFinderTest extends IntegrationTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->clearDb();
+    }
+
+    public function testEntitiesAreOrderedWithFinderMethod()
+    {
+        for ($i = 1000; $i >= 1; --$i) {
+            $user = new User(sprintf('User %d', $i));
+            $this->em->persist($user);
+        }
+        $this->em->flush();
+        $this->em->clear();
+        $this->assertNodesCount(1000);
+
+        $users = $this->em->getRepository(User::class)->findBy([], ['login' => 'ASC']);
+        $this->assertCount(1000, $users);
+
+        for ($i = 1; $i <= 1000; ++$i) {
+            $u = $users[$i];
+            $this->assertEquals(sprintf('User %d', $i), $u->getLogin());
+        }
+    }
+}
