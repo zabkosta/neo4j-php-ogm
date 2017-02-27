59d0f3330eab520e87221cb71740139b4ad0d6a6
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-02-27 08:32:18 +0100

    Removed extra file comment

--- src/EntityManagerInterface.php
+++ src/EntityManagerInterface.php
@@ -11,15 +11,6 @@
 
 namespace GraphAware\Neo4j\OGM;
 
-/*
- * This file is part of the GraphAware Neo4j PHP OGM package.
- *
- * (c) GraphAware Ltd <info@graphaware.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use Doctrine\Common\EventManager;
 use Doctrine\Common\Persistence\ObjectManager;
 
